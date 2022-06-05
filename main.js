e73fbd1b4f7c04829b2107d8df28ce32f34770aa
YoshiYodaFoster <noahbfoster@gmail.com>
2022-06-05 01:33:10 -0400

    added 8ball and tarkovmappicker commands

--- main.js
+++ main.js
@@ -92,9 +92,16 @@ client.on("messageCreate", async msg => {
       break;
 
     case ".jimmerhelp":
-      msg.channel.send("Commands (use prefix '"+prefix+"'):")
-      msg.channel.send("generate: sends three randomly generated passwords of different types to your DMs")
-      msg.channel.send("vote #: votes for option # if there is an ongoing vote.")
+      {
+        let toSend = ""
+        toSend+=("Commands:")
+        toSend+=("\n.generate: sends three randomly generated passwords of different types to your DMs")
+        toSend+=("\n.vote #: votes for option # if there is an ongoing vote.")
+        toSend+=("\n.tarkovmappicker: will return a random tarkov map")
+        toSend+=("\n.8ball: gives you a response as from a magic 8 ball")
+        msg.channel.send(toSend)
+      }
+
       break;
     case ".bartest":
       msg.channel.send("Loading bar test:")
@@ -130,6 +137,20 @@ client.on("messageCreate", async msg => {
     case ".newlinetest":
       msg.reply("test\ntest")
       break;
+    case ".tarkovmappicker":
+      {
+        let maps = ['Customs','Factory','Shoreline','Labs','Reserve','Lighthouse','Woods','Interchange']
+        let choice = maps[Math.floor(Math.random()*maps.length)]
+        msg.reply("Map choice: "+choice)
+        break;
+      }
+    case ".8ball":
+      {
+        let responses = ['It is certain.','It is decidedly so.','Without a doubt.','Yes definitely.','You may rely on it.','As I see it, yes.','Most likely.','Outlook good.', 'Yes.', 'Signs point to yes.', 'Reply hazy, try again.', 'Ask again later.', 'Better not tell you now.', 'Cannot predict now.', 'Concentrate and ask again.', "Don't count on it.", 'My reply is no.', 'My sources say no.', "Outlook not so good.", "Very doubtful."]
+        let choice = responses[Math.floor(Math.random()*responses.length)]
+        msg.reply(choice)
+        break;
+      }
       
 
     
@@ -186,6 +207,7 @@ client.on("messageCreate", async msg => {
     }
   }
   
+
 })
 
 
