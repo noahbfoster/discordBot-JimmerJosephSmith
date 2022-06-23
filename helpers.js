7a2a7e5326ebd44e45b40e47293be5e7965b70a1
YoshiYodaFoster <noahbfoster@gmail.com>
2022-06-23 00:45:33 -0400

    initial helper file

---
+++ helpers.js
@@ -0,0 +1,24 @@
+const admins = ["282614964840169472", "191086797126762496", "832731781231804447"] // these are strings as a workaround. includes() didn't work on the integers. weird, right?
+
+
+function isAdmin(user) {
+    if (admins.includes(""+user.id)) {
+      return true;
+    } else {
+      return false;
+    }
+}
+
+async function returnAdmins(interaction) {
+    var adminNames = []
+    admins.forEach(async element => {
+        await interaction.client.users.fetch(element).then((user)=> {
+            adminNames.push(user.username)
+        })
+    });
+    return adminNames
+}
+
+
+exports.isAdmin = isAdmin
+exports.returnAdmins = returnAdmins
