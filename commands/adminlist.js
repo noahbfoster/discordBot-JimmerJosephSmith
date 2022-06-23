e698dc71d474ae4e3e3eb323673063f411193c73
YoshiYodaFoster <noahbfoster@gmail.com>
2022-06-23 00:45:45 -0400

    added adminlist command

---
+++ commands/adminlist.js
@@ -0,0 +1,19 @@
+const { SlashCommandBuilder } = require('@discordjs/builders');
+const helpers = require('../helpers.js')
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('adminlist')
+        .setDescription('reply with a list of admins'),
+    async execute(interaction) {
+        await helpers.returnAdmins(interaction)
+        const admins = await helpers.returnAdmins(interaction)
+        var message = "Admins are: "
+        var adminsString = ""
+        admins.forEach(e => {
+            adminsString = adminsString + ", " + e
+        });
+        adminsString = adminsString.slice(2)
+        interaction.reply(message+adminsString)
+    }
+}
