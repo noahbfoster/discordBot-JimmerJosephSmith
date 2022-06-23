2a3ca2808e489dfab8ef4734a8de0644eecb49d1
YoshiYodaFoster <noahbfoster@gmail.com>
2022-06-23 00:07:10 -0400

    created a blank command file for reference later

--- blankcommand.js
+++ blankcommand.js
@@ -8,3 +8,14 @@ module.exports = {
         
     }
 }
+
+/* command with a target:
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('')
+        .setDescription('')
+        .addUserOption(option => option.setName('target').setDescription('the target user')),
+    async execute(interaction) {
+        
+    }
+} */
