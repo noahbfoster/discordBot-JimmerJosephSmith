89a6992838139a2a20f9857f105c9b00c6877bd4
YoshiYodaFoster <noahbfoster@gmail.com>
2022-09-03 18:41:00 -0400

    added wildmagic command for dnd

---
+++ commands/wildmagic.js
@@ -0,0 +1,14 @@
+const { SlashCommandBuilder } = require('@discordjs/builders');
+var wildMagicTable = require('../wildmagic.json');
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('wildmagic')
+        .setDescription('Rolls on the Wild Magic Table'),
+    async execute(interaction) {
+        randomIndex = Math.floor(Math.random()*wildMagicTable.length)
+        d100Roll = randomIndex + 1;
+        messageText = "You rolled a "+d100Roll+". The effect of that roll is: "+wildMagicTable[randomIndex];
+        interaction.reply({content: messageText, ephemeral: true});
+    }
+}
