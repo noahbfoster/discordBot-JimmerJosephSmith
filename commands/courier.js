f8e8e950f44a600750b03bca6ecf6c9b686ae89b
Noah Foster <noahbfoster@gmail.com>
2025-02-03 16:39:48 -0500

    Error handling

--- commands/courier.js
+++ commands/courier.js
@@ -11,19 +11,31 @@ module.exports = {
     async execute(interaction) {
         const user = interaction.options.getUser('target')
         const message = interaction.options.getString('message')
-        interaction.reply("Sending message to "+user.username)
-        courier(user, interaction.user, message)
+        var reply = ""
+        try {
+            await courier(user, interaction.user, message)
+            reply = "Sending message to "+user.username
+        } catch (e) {
+            // console.log(e)
+            if (e.message == "MaxLength") {
+                reply = "Your message was too long"
+            } else {
+                reply = "An error occurred"
+            }
+        }
+        interaction.reply(reply)
     }
 }
 
 async function courier(user, from, message) {
     messageToSend = "I've been looking for you. Got something I'm supposed to deliver, your hands only...\n\n" + "*A letter from " + from.username +":*\n```" + message + "```" 
     if (messageToSend.length > 2000) {
-        from.send("Your message was too long and failed to send")
+        // from.send("Your message was too long and failed to send")
+        throw(
+            new Error("MaxLength")
+        );
     } else {
         user.send(messageToSend)
+        console.log("Sending to "+user.username+"from "+ from.username + ": "+messageToSend)
     }
-    
-    //user.send(messageToSend)
-    console.log("Sending to "+user.username+"from "+ from.username + ": "+messageToSend)
 }
